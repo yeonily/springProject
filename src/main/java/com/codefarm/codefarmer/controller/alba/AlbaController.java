b72ca383bbb9dda3358169f899f564ae9408c94a
xogwan <xogwan@gmail.com>
2022-12-07 02:01:40 +0900

    20221206 연태관 / 알바 백 작업중

--- src/main/java/com/codefarm/codefarmer/controller/alba/AlbaController.java
+++ src/main/java/com/codefarm/codefarmer/controller/alba/AlbaController.java
@@ -1,19 +1,33 @@
 package com.codefarm.codefarmer.controller.alba;
 
 import com.codefarm.codefarmer.domain.alba.AlbaDTO;
+import com.codefarm.codefarmer.entity.admin.Policy;
 import com.codefarm.codefarmer.entity.alba.Alba;
+import com.codefarm.codefarmer.repository.alba.AlbaRepository;
+import com.codefarm.codefarmer.service.alba.AlbaDetailService;
 import com.codefarm.codefarmer.service.alba.AlbaListService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoField;
 import java.util.List;
 
 @Controller
@@ -23,13 +37,22 @@ import java.util.List;
 public class AlbaController {
 
     private final AlbaListService albaListService;
+    private final AlbaDetailService albaDetailService;
+    private final AlbaRepository albaRepository;
 
     @GetMapping("/list")
-    public void albaList(Model model) {
+    public void albaList(Model model, @PageableDefault(size = 10, sort = "AlbaId", direction = Sort.Direction.DESC) Pageable pageable) {
         log.info("들어옴1");
         model.addAttribute("lists", albaListService.showTop8ByOOrderByAlbaApplyEndDateDesc());
         model.addAttribute("counts",albaListService.showAlbaTotalCount());
         model.addAttribute("recents",albaListService.showListByRecent());
+
+        Page<Alba> albas = albaListService.albaShowAll(pageable);
+
+        model.addAttribute("maxPage", 5);
+        model.addAttribute("albas", albas);
+        log.info("진짜?");
+
     }
 
     @GetMapping("/write")
@@ -38,17 +61,48 @@ public class AlbaController {
         }
         
     @PostMapping("/write")
-    public RedirectView albaWrite(AlbaDTO albaDTO, RedirectAttributes redirectAttributes){
+    public RedirectView albaWrite(AlbaDTO albaDTO, String albaApplyStartDateString, String albaApplyEndDateString, String albaWorkDateString) throws DateTimeParseException {
         log.info("들어왔니?");
+        log.info("리스폰스바디 컨트롤러 들어옴");
+        log.info("albaApplyStartDateString:"+ albaApplyStartDateString);
+        log.info("albaApplyEndDateString:"+ albaApplyEndDateString);
+        log.info("albaWorkDateString:"+ albaWorkDateString);
+        log.info("albaDTO: " + albaDTO.toString());
 
-        log.info(albaDTO.toString());
-        albaListService.add(albaDTO);
-        redirectAttributes.addFlashAttribute("albaId", albaDTO.getAlbaId());
-        return new RedirectView("/list");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        DateTimeFormatter formatter1 = new DateTimeFormatterBuilder()
+                .append(DateTimeFormatter.ISO_LOCAL_TIME)
+                .parseDefaulting(ChronoField.EPOCH_DAY, 0)
+                .toFormatter();
+
+        LocalDateTime albaWorkDateTest = LocalDate.parse(albaWorkDateString, formatter).atStartOfDay();
+        log.info("1" + albaWorkDateTest);
+
+        LocalDateTime albaApplyStartDateTest = LocalDate.parse(albaApplyStartDateString, formatter).atStartOfDay();
+        log.info("2" + albaApplyStartDateTest);
+
+        LocalDateTime albaApplyEndDateTest = LocalDate.parse(albaApplyEndDateString, formatter).atStartOfDay();
+        log.info("3" + albaApplyEndDateTest);
+
+        albaDTO.setAlbaApplyStartDate(albaApplyStartDateTest);
+        albaDTO.setAlbaApplyEndDate(albaApplyEndDateTest);
+        albaDTO.setAlbaWorkDate(albaWorkDateTest);
+
+        albaListService.saveAll(albaDTO);
+
+//        redirectAttributes.addFlashAttribute("albaId", albaDTO.getAlbaId());
+        return new RedirectView("list");
     }
 
     @GetMapping("/detail")
-    public void albaDetail() {
+    public void albaDetail(Model model, @RequestParam Long albaId) {
+        log.info("디테일 들어옴");
+        log.info("alba : "+ albaId);
 
+        AlbaDTO list = albaDetailService.showByAlbaId(albaId);
+        model.addAttribute("list",list);
     }
+
+
 }
--- src/main/java/com/codefarm/codefarmer/service/alba/AlbaDetailService.java
+++ src/main/java/com/codefarm/codefarmer/service/alba/AlbaDetailService.java
@@ -2,6 +2,8 @@ package com.codefarm.codefarmer.service.alba;
 
 import com.codefarm.codefarmer.domain.alba.AlbaDTO;
 import com.codefarm.codefarmer.domain.alba.QAlbaDTO;
+import com.codefarm.codefarmer.domain.program.ProgramDTO;
+import com.codefarm.codefarmer.domain.program.QProgramDTO;
 import com.codefarm.codefarmer.entity.alba.Alba;
 import com.codefarm.codefarmer.repository.alba.AlbaRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 import static com.codefarm.codefarmer.entity.alba.QAlba.alba;
+import static com.codefarm.codefarmer.entity.program.QProgram.program;
 
 @Service
 @RequiredArgsConstructor
@@ -49,5 +52,40 @@ public class AlbaDetailService {
                 alba.albaProfileContent2
         )).from(alba)
                 .fetch();
+    }
+
+    public AlbaDTO showByAlbaId(Long albaId) {
+
+        return jpaQueryFactory.select(new QAlbaDTO(
+                alba.albaId,
+                alba.albaTitle,
+                alba.albaImage,
+                alba.albaTitleOne,
+                alba.albaApplyStartDate,
+                alba.albaApplyEndDate,
+                alba.albaWorkDate,
+                alba.albaApplyCount,
+                alba.albaApplyTotalCount,
+                alba.albaAddress,
+                alba.albaPrice,
+                alba.albaMainTitle,
+                alba.albaMainContent,
+                alba.albaStrongTitle1,
+                alba.albaStrongContent1,
+                alba.albaStrongTitle2,
+                alba.albaStrongContent2,
+                alba.albaStrongTitle3,
+                alba.albaStrongContent3,
+                alba.albaBannerTitle,
+                alba.albaBannerOne,
+                alba.albaTextTitle,
+                alba.albaText,
+                alba.albaProfileTitle1,
+                alba.albaProfileContent1,
+                alba.albaProfileTitle2,
+                alba.albaProfileContent2
+        )).from(alba)
+                .where(alba.albaId.eq(albaId))
+                .fetchOne();
     }
 }
