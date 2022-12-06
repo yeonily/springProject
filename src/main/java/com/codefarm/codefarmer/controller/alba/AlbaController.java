7fbfcf37b2f19c3a7e62701dd20f2358c5992402
xogwan <xogwan@gmail.com>
2022-12-06 09:03:23 +0900

    20221206 연태관 / 알바글 등록 작업중

--- src/main/java/com/codefarm/codefarmer/controller/alba/AlbaController.java
+++ src/main/java/com/codefarm/codefarmer/controller/alba/AlbaController.java
@@ -1,12 +1,18 @@
 package com.codefarm.codefarmer.controller.alba;
 
+import com.codefarm.codefarmer.domain.alba.AlbaDTO;
+import com.codefarm.codefarmer.entity.alba.Alba;
 import com.codefarm.codefarmer.service.alba.AlbaListService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -18,8 +24,6 @@ public class AlbaController {
 
     private final AlbaListService albaListService;
 
-
-
     @GetMapping("/list")
     public void albaList(Model model) {
         log.info("들어옴1");
@@ -29,8 +33,18 @@ public class AlbaController {
     }
 
     @GetMapping("/write")
-    public void albaWrite() {
+    public void albaWrite(Model model) {
+            model.addAttribute("alba", new AlbaDTO());
+        }
+        
+    @PostMapping("/write")
+    public RedirectView albaWrite(AlbaDTO albaDTO, RedirectAttributes redirectAttributes){
+        log.info("들어왔니?");
 
+        log.info(albaDTO.toString());
+        albaListService.add(albaDTO);
+        redirectAttributes.addFlashAttribute("albaId", albaDTO.getAlbaId());
+        return new RedirectView("/list");
     }
 
     @GetMapping("/detail")
--- src/main/java/com/codefarm/codefarmer/entity/alba/Alba.java
+++ src/main/java/com/codefarm/codefarmer/entity/alba/Alba.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "TBL_ALBA")
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class Alba extends Period {
     @Id @GeneratedValue
     private Long albaId;
