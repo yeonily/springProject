bf6412c3dfc03c256c970d51b39bb4beb72d64d1
xogwan <xogwan@gmail.com>
2022-12-07 16:54:38 +0900

    20221207 연태관 / 공사 전 푸쉬

--- src/main/java/com/codefarm/codefarmer/repository/alba/AlbaRepository.java
+++ src/main/java/com/codefarm/codefarmer/repository/alba/AlbaRepository.java
@@ -1,11 +1,11 @@
 package com.codefarm.codefarmer.repository.alba;
 
 import com.codefarm.codefarmer.entity.alba.Alba;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 
 public interface AlbaRepository extends JpaRepository<Alba, Long>, AlbaCustomRepository {
-    public List<Alba> findTop8ByOrderByAlbaApplyEndDateDesc();
 }
--- src/main/resources/static/js/alba/list.js
+++ src/main/resources/static/js/alba/list.js
@@ -20,6 +20,31 @@ for(var i = 0; i < $status.length; i++) {
     }
 }
 
+
+// $(document).ready(function () {
+//     $.ajax({
+//         url: "/alba/list",
+//         type: "post",
+//         success: function (lists) {
+//             let text = "";
+//
+//             lists.forEach(function (list) {
+//
+//                 text += '<div class="alba_div" onclick="location.href=" th:object="${list}">'
+//                 text += '<img src="https://cdn.comento.kr/edu/title_mmVkR6Q0Mz.jpeg?s=518x272&q=75">'
+//                 text += '<div>' + list.albaApplyCount + '/' + 'list.albaApplyCount' + '명' + '・' + '<span>' + list.albaPrice + '원' +'</span></div>'
+//                 // text += <div><span style="color: black" th:text="${list.albaApplyCount}"/>/<span style="color: black" th:text="${list.albaApplyTotalCount}"/>명 <span style="color: rgb(238, 126, 65)" >・ <span th:text="${list.albaPrice}"/>원</span></div>
+//                 text += '<p class="alba_title">' + list.albaTitle + '</p>'
+//                 text += '</div>'
+//                 text += '</div>'
+//                 text += '</div>'
+//             });
+//             $("#albaList").html(text);
+//         }
+// });
+// });
+
+
 /*-----------------------------------------------------------*/
                     //클릭 시 체크 확인 플레그
 /-----------------------------------------------------------*/
@@ -44,15 +69,15 @@ $("#recent-btn").on("click", function () {
         // data: formData,
         // contentType: false,
         // processData: false,
-        success: function (lists) {
+        success: function (recents) {
             let text = "";
 
-            lists.forEach(function (list) {
-                text += '<div class="list_td" th:object="${list}">'
-                text += '<div class="location"><p>' + list.albaAddress + '</p></div>'
-                text += '<div class="title"><a th:href="@{/alba/detail}">' + list.albaTitle + '(<span>모집중</span>)</a></div>'
+            recents.forEach(function (recent) {
+                text += '<div class="list_td" th:object="${recent}">'
+                text += '<div class="location"><p>' + recent.albaAddress + '</p></div>'
+                text += '<div class="title"><a th:href="@{/alba/detail}">' + recent.albaTitle + '</a></div>'
 
-                let albaWorkDate = new Date(list.albaWorkDate);
+                let albaWorkDate = new Date(recent.albaWorkDate);
                 let albaWorkYear = albaWorkDate.getFullYear();
                 let albaWorkMonth = albaWorkDate.getMonth()+1;
                 let albaWorkDay = albaWorkDate.getDate();
@@ -60,46 +85,52 @@ $("#recent-btn").on("click", function () {
                 text += '<div class="workDay">' + albaWorkYear + '년' + albaWorkMonth + '월' + albaWorkDay + '일' + '</div>'
                 text += '<div class="pay">'
                 text += '<p class="miniText">시급</p>'
-                text += '<div>' + list.albaPrice + '</div>'
+                text += '<div>' + recent.albaPrice + '</div>'
                 text += '</div>'
 
-                let albaStartDate = new Date(list.albaApplyStartDate);
+                let albaStartDate = new Date(recent.albaApplyStartDate);
                 let albaStartYear = albaStartDate.getFullYear();
                 let albaStartMonth = albaStartDate.getMonth()+1;
                 let albaStartDay = albaStartDate.getDate();
 
                 text += '<div class="registerDay">' + albaStartYear + '년' + albaStartMonth + '월' + albaStartDay + '일' + '</div>'
                 text += '</div>'
-                $("#list-foreach").html(text);
-
-
                 // 페이징 처리
 
-                // text += '<div id="paging">'
-                // text += '<ul th:with="start=${(albas.number/maxPage)*maxPage + 1}, end=(${(albas.totalPages == 0) ? 1 : (start + (maxPage - 1) < albas.totalPages ? start + (maxPage - 1) : albas.totalPages)})">'
-                // text += '<li className="sidePageNum prev" th:if="${albas.pageable.pageNumber > 0}">'
-                // text += '<a th:href="@{/alba/list(page=${((albas.pageable.pageNumber/5)*5) - 1})}" className="page">'
-                // text += '<svg width="16" height="16" className="sidePageDisabled" viewBox="0 0 16 16" fill="black" xmlns="http://www.w3.org/2000/svg" style="transform:rotate(270deg);" data-v-bd9f2bcc="" data-v-30c80689="">'
-                // text += '<path fill-rule="evenodd" clip-rule="evenodd" d="M7.64645 4.64645C7.84171 4.45118 8.15829 4.45118 8.35355 4.64645L12.8536 9.14645C13.0488 9.34171 13.0488 9.65829 12.8536 9.85355C12.6583 10.0488 12.3417 10.0488 12.1464 9.85355L8 5.70711L3.85355 9.85355C3.65829 10.0488 3.34171 10.0488 3.14645 9.85355C2.95118 9.65829 2.95118 9.34171 3.14645 9.14645L7.64645 4.64645Z "data-v-bd9f2bcc=""></path>
-                // text += '</svg>'
-                // text += '</a>'
-                // text += '</li>'
-                // text += '<th:block th:each="i: ${#numbers.sequence(start, end)}">'
-                // text += '<li>'
-                // text += '<a class="page selectPage" th:href="@{/alba/list(page=${i - 1})}" th:classappend="${i == albas.pageable.pageNumber + 1} ? 'selectPage' : '' "th:text="${i}">'
-                // text += '</a>'
-                // text += '</li>'
-                // text += '</th:block>'
-                // text += '<li className="sidePageNum next" th:if="${end < albas.totalPages}">'
-                // text += '<a th:href="@{/alba/list(page=${end})}" className="page">'
-                // text += '<svg width="16" height="16" viewBox="0 0 16 16" fill="black" xmlns="http://www.w3.org/2000/svg" style="transform:rotate(90deg);" data-v-bd9f2bcc="" data-v-30c80689="">'
-                // text += '<path fill-rule="evenodd" clip-rule="evenodd" d="M7.64645 4.64645C7.84171 4.45118 8.15829 4.45118 8.35355 4.64645L12.8536 9.14645C13.0488 9.34171 13.0488 9.65829 12.8536 9.85355C12.6583 10.0488 12.3417 10.0488 12.1464 9.85355L8 5.70711L3.85355 9.85355C3.65829 10.0488 3.34171 10.0488 3.14645 9.85355C2.95118 9.65829 2.95118 9.34171 3.14645 9.14645L7.64645 4.64645Z" data-v-bd9f2bcc="">'
-                // text += '</path>'
-                // text += '</svg>'
-                // text += '</a>'
-                // text += '</li>'
-                // text += '</ul>'
-                // text += '</div>'
+
+                // text += <div id="paging" style="margin-bottom: 100px;">
+                // text +=    <ul th:with="start=${(albas.number/maxPage)*maxPage + 1}, end=(${(albas.totalPages == 0) ? 1 : (start + (maxPage - 1) < albas.totalPages ? start + (maxPage - 1) : albas.totalPages)})">
+                // text +=         <li className="sidePageNum prev" th:if="${albas.pageable.pageNumber > 0}">
+                // text +=             <a th:href="@{/alba/list(page=${((albas.pageable.pageNumber/5)*5) - 1})}" className="page">
+                // text +=                 <svg width="16" height="16" className="sidePageDisabled" viewBox="0 0 16 16"
+                //                      fill="black" xmlns="http://www.w3.org/2000/svg" style="transform:rotate(270deg);"
+                //                      data-v-bd9f2bcc="" data-v-30c80689="">
+                // text +=                    <path fill-rule="evenodd" clip-rule="evenodd"
+                //                           d="M7.64645 4.64645C7.84171 4.45118 8.15829 4.45118 8.35355 4.64645L12.8536 9.14645C13.0488 9.34171 13.0488 9.65829 12.8536 9.85355C12.6583 10.0488 12.3417 10.0488 12.1464 9.85355L8 5.70711L3.85355 9.85355C3.65829 10.0488 3.34171 10.0488 3.14645 9.85355C2.95118 9.65829 2.95118 9.34171 3.14645 9.14645L7.64645 4.64645Z"
+                //                           data-v-bd9f2bcc=""></path>
+                //     text +=            </svg>
+                //     text +=        </a>
+                //     text +=    </li>
+                //     text +=    <th:block th:each="i: ${#numbers.sequence(start, end)}">
+                //     text +=        <li>
+                //     text +=            <a className="page" th:href="@{/alba/list(page=${i - 1})}"
+                //                    th:classappend="${i == albas.pageable.pageNumber + 1} ? 'selectPage' : '' "
+                //                    th:text="${i}">1</a>
+                //     text +=        </li>
+                //     text +=    <li className="sidePageNum next" th:if="${end < albas.totalPages}">
+                //     text +=        <a th:href="@{/alba/list(page=${end})}" className="page">
+                //     text +=            <svg width="16" height="16" viewBox="0 0 16 16" fill="black"
+                //                      xmlns="http://www.w3.org/2000/svg" style="transform:rotate(90deg);"
+                //                      data-v-bd9f2bcc="" data-v-30c80689="">
+                //     text +=                <path fill-rule="evenodd" clip-rule="evenodd"
+                //                           d="M7.64645 4.64645C7.84171 4.45118 8.15829 4.45118 8.35355 4.64645L12.8536 9.14645C13.0488 9.34171 13.0488 9.65829 12.8536 9.85355C12.6583 10.0488 12.3417 10.0488 12.1464 9.85355L8 5.70711L3.85355 9.85355C3.65829 10.0488 3.34171 10.0488 3.14645 9.85355C2.95118 9.65829 2.95118 9.34171 3.14645 9.14645L7.64645 4.64645Z"
+                //                           data-v-bd9f2bcc=""></path>
+                //     text +=            </svg>
+                //     text +=        </a>
+                //     text +=   </li>
+                //     text +=</ul>
+                //     text +=</div>
+            $("#list-foreach").html(text);
             })
         }
     });
@@ -131,7 +162,7 @@ $("#pay-btn").on("click", function () {
             lists.forEach(function (list) {
                 text += '<div class="list_td" th:object="${list}">'
                 text += '<div class="location"><p>' + list.albaAddress + '</p></div>'
-                text += '<div class="title"><a th:href="@{/alba/detail}">' + list.albaTitle + '(<span>모집중</span>)</a></div>'
+                text += '<div class="title"><a th:href="@{/alba/detail}">' + list.albaTitle + '</a></div>'
 
                 let albaWorkDate = new Date(list.albaWorkDate);
                 let albaWorkYear = albaWorkDate.getFullYear();
@@ -152,8 +183,8 @@ $("#pay-btn").on("click", function () {
                 text += '<div class="registerDay">' + albaStartYear + '년' + albaStartMonth + '월' + albaStartDay + '일' + '</div>'
                 text += '</div>'
                 $("#list-foreach").html(text);
-                })
-            }
+            })
+        }
     });
     check = true;
 })
@@ -162,7 +193,7 @@ $("#pay-btn").on("click", function () {
                     //모집중 정렬 클릭 시
 
 
-$("#progress-btn").on("click", function () {
+$("#deadline-btn").on("click", function () {
     // if(check == true){
     //     check = false;
     //     location.href = "/alba/list";
@@ -182,7 +213,7 @@ $("#progress-btn").on("click", function () {
             lists.forEach(function (list) {
                 text += '<div class="list_td" th:object="${list}">'
                 text += '<div class="location"><p>' + list.albaAddress + '</p></div>'
-                text += '<div class="title"><a th:href="@{/alba/detail}">' + list.albaTitle + '(<span>모집중</span>)</a></div>'
+                text += '<div class="title"><a th:href="@{/alba/detail}">' + list.albaTitle + '</a></div>'
 
                 let albaWorkDate = new Date(list.albaWorkDate);
                 let albaWorkYear = albaWorkDate.getFullYear();
