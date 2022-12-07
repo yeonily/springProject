a12cdfc54e37f643cdbab8c407509cddb157af8b
ParkSuiHyeon <dream3773@gmail.com>
2022-12-07 17:41:18 +0900

    221207 박수현 멤버 통합 오류 수정

--- src/test/java/com/codefarm/codefarmer/entity/admin/AdminTest.java
+++ src/test/java/com/codefarm/codefarmer/entity/admin/AdminTest.java
@@ -1,29 +1,26 @@
 package com.codefarm.codefarmer.entity.admin;
 
 import com.codefarm.codefarmer.domain.alba.AlbaDTO;
-import com.codefarm.codefarmer.domain.member.FarmerDTO;
-import com.codefarm.codefarmer.domain.member.UserDTO;
 import com.codefarm.codefarmer.domain.program.MemberProgramDTO;
 import com.codefarm.codefarmer.domain.program.ProgramDTO;
 import com.codefarm.codefarmer.entity.alba.Alba;
 import com.codefarm.codefarmer.entity.alba.QAlba;
-import com.codefarm.codefarmer.entity.member.Farmer;
+import com.codefarm.codefarmer.entity.member.Member;
 import com.codefarm.codefarmer.entity.member.QMember;
 import com.codefarm.codefarmer.entity.member.QUser;
-import com.codefarm.codefarmer.entity.member.User;
 import com.codefarm.codefarmer.entity.program.MemberProgram;
 import com.codefarm.codefarmer.entity.program.Program;
 import com.codefarm.codefarmer.entity.program.QMemberProgram;
 import com.codefarm.codefarmer.entity.program.QProgram;
 import com.codefarm.codefarmer.repository.alba.AlbaRepository;
 import com.codefarm.codefarmer.repository.alba.MemberAlbaRepository;
-import com.codefarm.codefarmer.repository.member.FarmerRepository;
-import com.codefarm.codefarmer.repository.member.UserRepository;
+import com.codefarm.codefarmer.repository.member.MemberRepository;
 import com.codefarm.codefarmer.repository.program.MemberProgramRepository;
 import com.codefarm.codefarmer.repository.program.ProgramRepository;
 import com.codefarm.codefarmer.type.*;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.usertype.UserType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -47,12 +44,10 @@ import static com.codefarm.codefarmer.entity.program.QProgram.program;
 @Rollback(false)
 public class AdminTest {
     @Autowired
-    private JPAQueryFactory jpaQueryFactory;
+    private JPAQueryFactory jpaQueryFacto;
+    @Autowired
+    private MemberRepository memberRepository;
 
-    @Autowired
-    private FarmerRepository farmerRepository;
-    @Autowired
-    private UserRepository userRepository;
 
     @Autowired
     private ProgramRepository programRepository;
@@ -64,41 +59,8 @@ public class AdminTest {
     @Autowired
     private MemberAlbaRepository memberAlbaRepository;
 
-//    농장주 유저 등록
-    @Test
-    public void farmerSaveTest(){
-        FarmerDTO farmerDTO = new FarmerDTO();
+//    유저 등록
 
-        farmerDTO.setMemberBirth("2000-03-10");
-        farmerDTO.setMemberEmail("spring@spring.java");
-        farmerDTO.setFarmerType(FarmerType.FARMER);
-        farmerDTO.setMemberLocation("서울시 송파구");
-        farmerDTO.setMemberName("박수현");
-        farmerDTO.setMemberNickname("주어머거");
-        farmerDTO.setMemberOauth(Oauth.NAVER);
-        farmerDTO.setMemberPhone("010-0000-0000");
-
-        Farmer farmer = farmerDTO.toEntity();
-        farmerRepository.save(farmer);
-    }
-
-//    일반 유저 등록
-    @Test
-    public void userSaveTest(){
-        UserDTO userDTO = new UserDTO();
-
-        userDTO.setMemberBirth("2000-03-13");
-        userDTO.setMemberEmail("spring@spring.java");
-        userDTO.setUserType(UserType.USER);
-        userDTO.setMemberLocation("서울시 강남구");
-        userDTO.setMemberName("홍길동");
-        userDTO.setMemberNickname("홍홍홍22");
-        userDTO.setMemberOauth(Oauth.GOOGLE);
-        userDTO.setMemberPhone("010-1111-1111");
-
-        User user = userDTO.toEntity();
-        userRepository.save(user);
-    }
 
 //    유저 삭제
     @Test
@@ -110,18 +72,9 @@ public class AdminTest {
     @Test
     public void memberSelectAllTest(){
 
-        jpaQueryFactory.select(member, user.userType, farmer.farmerType).from(member)
-                .leftJoin(user).on(user.eq(member))
-                .leftJoin(farmer).on(farmer.eq(member))
-                .fetch()
-                .stream().forEach(m -> log.info("member : " + m));
     }
 
 //    총 유저 인원 수
-    @Test
-    public void memberCountTest(){
-        log.info("member count : " + (farmerRepository.count() + userRepository.count()));
-    }
 
 //    ========================================================================================================
 
@@ -131,7 +84,7 @@ public class AdminTest {
         LocalDateTime localDateTime = LocalDateTime.now();
 
         ProgramDTO programDTO = new ProgramDTO();
-        Optional<Farmer> findFarmer = farmerRepository.findById(10L);
+        Optional<Member> findFarmer = memberRepository.findById(10L);
 
 //        programDTO.setMember(findFarmer.get());
         programDTO.setProgramCrop("블루베리");
@@ -167,7 +120,7 @@ public class AdminTest {
         programDTO.setProgramInquire(findFarmer.get().getMemberEmail());
 
         Program program = programDTO.toEntity();
-//        program.changeMember(programDTO.getMember());
+//        program.changeMember(programDTO.getMemberId().);
         programRepository.save(program);
     }
 
@@ -181,11 +134,6 @@ public class AdminTest {
     @Test
     public void programSelectAllTest(){
 //        programRepository.findAll().stream().map(program -> program.toString()).forEach(log::info);
-
-        jpaQueryFactory.select(program.programTitle, program.programTitleSub, program.programCrop, program.programPrice)
-                .from(program)
-                .fetch()
-                .stream().map(Program -> Program.toString()).forEach(log::info);
     }
 
 //    ========================================================================================================
@@ -194,10 +142,10 @@ public class AdminTest {
     @Test
     public void memberProgramSaveTest(){
         MemberProgramDTO memberProgramDTO = new MemberProgramDTO();
-        Optional<User> findUser = userRepository.findById(11L);
+        Optional<Member> findMember = memberRepository.findById(11L);
         Optional<Program> findProgram = programRepository.findById(16L);
 
-        memberProgramDTO.setMember(findUser.get());
+        memberProgramDTO.setMember(findMember.get());
         memberProgramDTO.setProgram(findProgram.get());
         memberProgramDTO.setProgramApplyCount(2);
         int totalCount = findProgram.get().getProgramPrice() * memberProgramDTO.getProgramApplyCount();
@@ -221,10 +169,6 @@ public class AdminTest {
     public void memberProgramSelectAllTest(){
 //        memberProgramRepository.findAll().stream().map(memberProgram -> memberProgram.toString()).forEach(log::info);
 
-        jpaQueryFactory.select(memberProgram.member.memberName, memberProgram.program.programTitle, memberProgram.programApplyCount, memberProgram.programStatus)
-                .from(memberProgram)
-                .fetch()
-                .stream().map(MemberProgram -> MemberProgram.toString()).forEach(log::info);
     }
 
 //    ========================================================================================================
@@ -235,7 +179,7 @@ public class AdminTest {
         LocalDateTime localDateTime = LocalDateTime.now();
 
         AlbaDTO albaDTO = new AlbaDTO();
-        Optional<Farmer> findFarmer = farmerRepository.findById(10L);
+        Optional<Member> findMember = memberRepository.findById(10L);
 
         albaDTO.setAlbaAddress("서울시 관악구");
         albaDTO.setAlbaApplyCount(123);
@@ -263,7 +207,7 @@ public class AdminTest {
         albaDTO.setAlbaTitle("연태관 알바제목");
         albaDTO.setAlbaTitleOne("연태관 알바제목원");
         albaDTO.setAlbaWorkDate(localDateTime);
-        albaDTO.setMember(findFarmer.get());
+        albaDTO.setMember(findMember.get());
 
         Alba alba = albaDTO.toEntity();
         alba.changeMember(albaDTO.getMember());
@@ -275,9 +219,6 @@ public class AdminTest {
     public void albaSelectAllTest(){
         albaRepository.findAll().stream().map(Alba -> Alba.toString()).forEach(log::info);
 
-        jpaQueryFactory.select(alba.albaTitle, alba.albaMainTitle).from(alba)
-                .fetch()
-                .stream().map(Alba -> Alba.toString()).forEach(log::info);
     }
 
 //    아르바이트 삭제
