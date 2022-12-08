65d7f3e671371ebfeca4edd265f2f6227a52f657
yeonily <wluds9791@gmail.com>
2022-12-09 01:08:44 +0900

    221208 밤 커밋

--- src/main/java/com/codefarm/codefarmer/service/join/JoinService.java
+++ src/main/java/com/codefarm/codefarmer/service/join/JoinService.java
@@ -1,6 +1,8 @@
 package com.codefarm.codefarmer.service.join;
 
+import com.codefarm.codefarmer.entity.member.Member;
 import com.codefarm.codefarmer.repository.member.MemberRepository;
+import com.codefarm.codefarmer.type.MemberType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +12,13 @@ public class JoinService {
 
     private final MemberRepository memberRepository;
 
+    //닉네임 중복체크
     public Integer checkUserNick(String nickname) {return memberRepository.checkNick(nickname);}
 
+    //회원가입 시 사용(세션저장용)
     public Long selectId(String oauthId){return memberRepository.findMemberId(oauthId);}
+    public String selectType(String oauthId){return memberRepository.findMemberType(oauthId);}
 
+    //디비에 oauthid있는지 검사(있으면 1 리턴)
     public Integer checkOauth(String oauthId){return memberRepository.checkOauth(oauthId);}
 }
