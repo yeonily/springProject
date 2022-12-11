fa2c0932bcfd8871757925c2dd243c6144b18bb4
xogwan <xogwan@gmail.com>
2022-12-12 03:06:15 +0900

    20221211 연태관 / 지원하기 작업중

--- src/main/java/com/codefarm/codefarmer/service/alba/AlbaDetailService.java
+++ src/main/java/com/codefarm/codefarmer/service/alba/AlbaDetailService.java
@@ -1,14 +1,18 @@
 package com.codefarm.codefarmer.service.alba;
 
 import com.codefarm.codefarmer.domain.alba.AlbaDTO;
+import com.codefarm.codefarmer.domain.alba.MemberAlbaDTO;
 import com.codefarm.codefarmer.domain.alba.QAlbaDTO;
 import com.codefarm.codefarmer.domain.program.ProgramDTO;
 import com.codefarm.codefarmer.domain.program.QProgramDTO;
+import com.codefarm.codefarmer.entity.admin.Crop;
 import com.codefarm.codefarmer.entity.alba.Alba;
 import com.codefarm.codefarmer.entity.alba.QAlba;
+import com.codefarm.codefarmer.entity.inquire.Inquire;
 import com.codefarm.codefarmer.entity.mentor.MentorBoard;
 import com.codefarm.codefarmer.entity.program.Program;
 import com.codefarm.codefarmer.repository.alba.AlbaRepository;
+import com.codefarm.codefarmer.repository.alba.MemberAlbaRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -25,6 +29,7 @@ public class AlbaDetailService {
 
     private final JPAQueryFactory jpaQueryFactory;
     private final AlbaRepository albaRepository;
+    private final MemberAlbaRepository memberAlbaRepository;
 
     public List<AlbaDTO> showAll(){
         return jpaQueryFactory.select(new QAlbaDTO(
@@ -96,91 +101,23 @@ public class AlbaDetailService {
                 .fetchOne();
     }
 
-
-    //    프로그램Id로 수정 페이지 내용 출력
-    public AlbaDTO showUpdate(Long albaId){
-        return jpaQueryFactory.select(new QAlbaDTO(
-                alba.albaId,
-                alba.albaTitle,
-                alba.albaImage,
-                alba.albaTitleOne,
-                alba.albaApplyStartDate,
-                alba.albaApplyEndDate,
-                alba.albaWorkDate,
-                alba.albaApplyCount,
-                alba.albaApplyTotalCount,
-                alba.albaAddress,
-                alba.albaPrice,
-                alba.albaMainTitle,
-                alba.albaMainContent,
-                alba.albaStrongTitle1,
-                alba.albaStrongContent1,
-                alba.albaStrongTitle2,
-                alba.albaStrongContent2,
-                alba.albaStrongTitle3,
-                alba.albaStrongContent3,
-                alba.albaBannerTitle,
-                alba.albaBannerOne,
-                alba.albaTextTitle,
-                alba.albaText,
-                alba.albaProfileTitle1,
-                alba.albaProfileContent1,
-                alba.albaProfileTitle2,
-                alba.albaProfileContent2,
-                alba.member.memberId
-        )).from(alba)
-                .where(alba.albaId.eq(albaId))
-                .fetchOne();
-    }
-
     // 알바 게시글 수정
     public void albaUpdate(AlbaDTO albaDTO) {
-        albaRepository.save(albaDTO.toEntity());
+        Alba alba = albaDTO.toEntity();
+        alba.update(albaDTO);
+        albaRepository.deleteById(alba.getAlbaId());
+        albaRepository.save(alba);
     }
-
-    public AlbaDTO albaShowOne(Long albaId){
-
-        Alba alba = albaRepository.findById(albaId).get();
-
-
-        AlbaDTO albaDTO = new AlbaDTO();
-
-        albaDTO.setAlbaImage(alba.getAlbaImage());
-        albaDTO.setAlbaAddress(alba.getAlbaAddress());
-        albaDTO.setAlbaId(alba.getAlbaId());
-        albaDTO.setAlbaApplyCount(alba.getAlbaApplyCount());
-        albaDTO.setAlbaApplyEndDate(alba.getAlbaApplyEndDate());
-        albaDTO.setAlbaApplyStartDate(alba.getAlbaApplyStartDate());
-        albaDTO.setAlbaApplyTotalCount(alba.getAlbaApplyTotalCount());
-        albaDTO.setAlbaBannerOne(alba.getAlbaBannerOne());
-        albaDTO.setAlbaBannerTitle(alba.getAlbaBannerTitle());
-        albaDTO.setAlbaMainContent(alba.getAlbaMainContent());
-        albaDTO.setAlbaMainTitle(alba.getAlbaMainTitle());
-        albaDTO.setAlbaPrice(alba.getAlbaPrice());
-        albaDTO.setAlbaProfileContent1(alba.getAlbaProfileContent1());
-        albaDTO.setAlbaProfileContent2(alba.getAlbaProfileContent2());
-        albaDTO.setAlbaProfileTitle1(alba.getAlbaProfileTitle1());
-        albaDTO.setAlbaProfileTitle2(alba.getAlbaProfileTitle2());
-        albaDTO.setAlbaStrongContent1(alba.getAlbaStrongContent1());
-        albaDTO.setAlbaStrongContent2(alba.getAlbaStrongContent2());
-        albaDTO.setAlbaStrongContent3(alba.getAlbaStrongContent3());
-        albaDTO.setAlbaStrongTitle1(alba.getAlbaStrongTitle1());
-        albaDTO.setAlbaStrongTitle2(alba.getAlbaStrongTitle2());
-        albaDTO.setAlbaStrongTitle3(alba.getAlbaStrongTitle3());
-        albaDTO.setAlbaText(alba.getAlbaText());
-        albaDTO.setAlbaTextTitle(alba.getAlbaTextTitle());
-        albaDTO.setAlbaTitle(alba.getAlbaTitle());
-        albaDTO.setAlbaTitleOne(alba.getAlbaTitleOne());
-        albaDTO.setAlbaWorkDate(alba.getAlbaWorkDate());
-        albaDTO.setMemberId(alba.getMember().getMemberId());
-
-        return albaDTO;
-    }
-
 
     // 알바 게시글 삭제하기
     public Long removeAlbaId(Long albaId){
         albaRepository.deleteById(albaId);
         return albaId;
+    }
+
+    // 지원 신청하기
+    public void albaApply(MemberAlbaDTO memberAlbaDTO){
+        memberAlbaRepository.save(memberAlbaDTO.toEntity());
+
     }
 }
---
+++ src/test/java/com/codefarm/codefarmer/entity/alba/MemberAlbaTest.java
@@ -0,0 +1,63 @@
+package com.codefarm.codefarmer.entity.alba;
+
+import com.codefarm.codefarmer.domain.alba.MemberAlbaDTO;
+import com.codefarm.codefarmer.entity.member.Member;
+import com.codefarm.codefarmer.repository.alba.AlbaRepository;
+import com.codefarm.codefarmer.repository.alba.MemberAlbaRepository;
+import com.codefarm.codefarmer.repository.member.MemberRepository;
+import com.codefarm.codefarmer.type.Status;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static com.codefarm.codefarmer.entity.alba.QMemberAlba.memberAlba;
+
+@SpringBootTest
+@Slf4j
+@Transactional
+@Rollback(false)
+public class MemberAlbaTest {
+    @Autowired
+    private JPAQueryFactory jpaQueryFactory;
+
+    @Autowired
+    MemberAlbaRepository memberAlbaRepository;
+
+    @Autowired
+    AlbaRepository albaRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void saveTest(){
+        MemberAlbaDTO memberAlbaDTO = new MemberAlbaDTO();
+        Optional<Member> findUser = memberRepository.findById(131L);
+        Optional<Alba> findAlba = albaRepository.findById(132L);
+        memberAlbaDTO.setMemberStatus(Status.CONFIRM);
+        memberAlbaDTO.setMemberId(findUser.get());
+        memberAlbaDTO.setAlba(findAlba.get());
+        MemberAlba memberAlba = memberAlbaDTO.toEntity();
+        memberAlba.changeMember(memberAlbaDTO.getMemberId());
+        memberAlba.changeAlba(memberAlbaDTO.getAlba());
+        memberAlbaRepository.save(memberAlba);
+    }
+
+    @Test
+    public void findAllTest(){
+        jpaQueryFactory.selectFrom(memberAlba)
+                .fetch()
+                .stream().map(memberAlba -> memberAlba.toString()).forEach(log::info);
+    }
+
+    @Test
+    public void deleteTest(){
+        memberAlbaRepository.deleteById(132L);
+    }
+}
