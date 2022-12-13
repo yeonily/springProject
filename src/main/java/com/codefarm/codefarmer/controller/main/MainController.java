504c69ee1a9ff0bb07eda4cec6fb7cc1119d5ca7
ssw4688 <tmddn4688@gmail.com>
2022-12-14 01:30:08 +0900

    메인페이지 커뮤니티 작업중

--- src/main/java/com/codefarm/codefarmer/controller/main/MainController.java
+++ src/main/java/com/codefarm/codefarmer/controller/main/MainController.java
@@ -2,6 +2,8 @@ package com.codefarm.codefarmer.controller.main;
 
 import com.codefarm.codefarmer.service.alba.AlbaListService;
 import com.codefarm.codefarmer.service.alba.AlbaService;
+import com.codefarm.codefarmer.service.board.BoardService;
+import com.codefarm.codefarmer.service.board.ReplyService;
 import com.codefarm.codefarmer.service.program.ProgramListService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,11 +20,15 @@ public class MainController {
 
     private final AlbaListService albaListService;
     private final ProgramListService programListService;
+    private final BoardService boardService;
+    private final ReplyService replyService;
 
     @GetMapping("main")
     public void getAlbaList(Model model) {
         model.addAttribute("albas", albaListService.showListByRecentEndDate());
         model.addAttribute("programs", programListService.findTop8ByOrderByProgramApplyEndDateDesc());
+        model.addAttribute("boards", boardService.getBoardList());
+        model.addAttribute("replies", replyService.getReplyList());
     }
 
 
