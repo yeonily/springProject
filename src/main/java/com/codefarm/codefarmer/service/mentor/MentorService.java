2d9c850fcea444a9c01f4f46ff962252fb4f369b
ssw4688 <tmddn4688@gmail.com>
2022-12-15 08:34:01 +0900

    멘토 삭제완료, 수정 오류발생, 멘토 인트로 페이지 완료

--- src/main/java/com/codefarm/codefarmer/service/mentor/MentorService.java
+++ src/main/java/com/codefarm/codefarmer/service/mentor/MentorService.java
@@ -1,15 +1,9 @@
 package com.codefarm.codefarmer.service.mentor;
 
-import com.codefarm.codefarmer.domain.mentor.MentorBoardDTO;
-import com.codefarm.codefarmer.domain.mentor.MentorFileDTO;
-import com.codefarm.codefarmer.domain.mentor.QMentorBoardDTO;
-import com.codefarm.codefarmer.domain.mentor.QMentorFileDTO;
+import com.codefarm.codefarmer.domain.mentor.*;
 import com.codefarm.codefarmer.domain.program.ProgramFileDTO;
 import com.codefarm.codefarmer.domain.program.QProgramFileDTO;
-import com.codefarm.codefarmer.entity.mentor.Mentor;
-import com.codefarm.codefarmer.entity.mentor.MentorBoard;
-import com.codefarm.codefarmer.entity.mentor.QMentorBoard;
-import com.codefarm.codefarmer.entity.mentor.QMentorFile;
+import com.codefarm.codefarmer.entity.mentor.*;
 import com.codefarm.codefarmer.repository.member.MemberRepository;
 import com.codefarm.codefarmer.repository.mentor.MentorBoardRepository;
 import com.codefarm.codefarmer.repository.mentor.MentorFileRepository;
@@ -24,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static com.codefarm.codefarmer.entity.mentor.QMentor.mentor;
 import static com.codefarm.codefarmer.entity.mentor.QMentorBoard.mentorBoard;
 import static com.codefarm.codefarmer.entity.mentor.QMentorFile.mentorFile;
 import static com.codefarm.codefarmer.entity.program.QProgramFile.programFile;
@@ -43,6 +38,7 @@ public class MentorService {
         log.info("mentorBoardDTO:"+mentorBoardDTO.toString());
         MentorBoard mentorBoard = mentorBoardDTO.toEntity();
         mentorBoard.changeFiles(mentorBoardDTO.getFiles());
+        mentorBoard.changeMember(memberRepository.findById(mentorBoardDTO.getMemberId()).get());
         mentorBoard.changeMentor(mentorRepository.findById(mentorBoardDTO.getMentorId()).get());
         mentorBoardRepository.save(mentorBoard);
         mentorBoard.getMentorFiles().stream().map(t -> mentorFileRepository.save(t));
@@ -72,7 +68,6 @@ public class MentorService {
         return mentorBoardDTO;
     }
 
-//    목록 들고오기
     public List<MentorBoardDTO> getMentorList(){
         return jpaQueryFactory.select(new QMentorBoardDTO(
                 mentorBoard.mentorBoardId,
@@ -94,16 +89,59 @@ public class MentorService {
                 .fetch();
     }
 
+    public List<MentorBoardDTO> getIntroMentor(){
+        return jpaQueryFactory.select(new QMentorBoardDTO(
+                mentorBoard.mentorBoardId,
+                mentorBoard.mentorTitle,
+                mentorBoard.member.memberNickname
+                )).from(mentorBoard).orderBy(mentorBoard.updatedDate.desc())
+                .limit(8)
+                .fetch();
+    }
+
+    public List<MentorBoardDTO> getInfo(){
+        return jpaQueryFactory.select(new QMentorBoardDTO(
+                mentorBoard.mentor.mentorCrop,
+                mentorBoard.mentor.mentorYear,
+                mentorBoard.member.memberLocation,
+                mentorBoard.member.memberNickname
+                )).from(mentorBoard)
+                .fetch();
+}
+
+public Long findmentorByBoardId(Long boardId){
+        return jpaQueryFactory.select(mentorBoard.member.memberId).from(mentorBoard)
+                .where(mentorBoard.mentorBoardId.eq(boardId))
+                .fetchOne();
+}
+
+public Long findmentoInfo(Long memberID){
+        return jpaQueryFactory.select(mentor.mentorId).from(mentor)
+                .where(mentor.member.memberId.eq(memberID)).fetchOne();
+}
+
+public MentorDTO findBymentoId(Long mentorId){
+        return jpaQueryFactory.select(new QMentorDTO(
+            mentor.mentorCrop,
+            mentor.mentorYear,
+            mentor.member.memberLocation,
+            mentor.member.memberNickname
+        )).from(mentor).where(mentor.mentorId.eq(mentorId)).fetchOne();
+}
+
+
     @Transactional(readOnly = true)
     public Page<MentorBoardDTO> showAll(Long mentorBoardId, Pageable pageable){
         return mentorBoardRepository.findAllByMentorBoardId(mentorBoardId, pageable);
     }
 
-//    멘토 목록에 닉네임 갖고오기
-//    멘토 목록에 제목 갖고오기  (닉넴이랑 제목 둘다 목록 한번에 묶을 필요있음)
 
 //    멘토보드 삭제하기
     public void removeMentorBoard(Long mentorBoardId){
+
+        List<MentorFile> mentorFiles = mentorFileRepository.findByMentorBoard_MentorBoardId(mentorBoardId);
+
+        mentorFiles.forEach(t -> mentorFileRepository.delete(t));
         mentorBoardRepository.deleteById(mentorBoardId);
     }
 
@@ -120,6 +158,49 @@ public List<MentorFileDTO> showFiles(Long mentorBoardId){
     )).from(mentorFile)
             .where(mentorFile.mentorBoard.mentorBoardId.eq(mentorBoardId)).fetch();
 }
+
+
+//    mentorBoardId로 수정 페이지 내용 들고오기
+    public MentorBoardDTO showUpdate(Long mentorBoardId){
+        return jpaQueryFactory.select(new QMentorBoardDTO(
+                mentorBoard.mentorBoardId,
+                mentorBoard.mentorCareer,
+                mentorBoard.mentorExCareer,
+                mentorBoard.mentorStrongTitle1,
+                mentorBoard.mentorStrongContent1,
+                mentorBoard.mentorStrongTitle2,
+                mentorBoard.mentorStrongContent2,
+                mentorBoard.mentorStrongTitle3,
+                mentorBoard.mentorStrongContent3,
+                mentorBoard.mentorTitle,
+                mentorBoard.mentorTitleSub,
+                mentorBoard.mentorTextTitle,
+                mentorBoard.mentorTextContent,
+                mentorBoard.member.memberId
+                 )).from(mentorBoard)
+                .where(mentorBoard.mentorBoardId.eq(mentorBoardId))
+                .fetchOne();
+    }
+
+//    수정 시 파일 삭제하고 다시 입력하면 저장하기
+    public void update(MentorBoardDTO mentorBoardDTO){
+        MentorBoard mentorBoard = mentorBoardDTO.toEntity();
+
+        mentorBoard.changeFiles(mentorBoardDTO.getFiles());
+        mentorBoard.changeMember(memberRepository.findById(mentorBoardDTO.getMemberId()).get());
+        mentorBoard.changeMentor(mentorRepository.findById(mentorBoardDTO.getMentorId()).get());
+
+        mentorBoard.getMentorFiles().stream().map(t -> mentorFileRepository.save(t));
+
+        List<MentorFile> mentorFiles = mentorFileRepository.findByMentorBoard_MentorBoardId(mentorBoardDTO.getMentorBoardId());
+        mentorFiles.forEach(t -> mentorFileRepository.delete(t));
+
+        List<MentorFileDTO> files = mentorBoardDTO.getFiles();
+        files.stream().map(t -> t.toEntity());
+
+        mentorBoard.update(mentorBoardDTO);
+        mentorBoardRepository.save(mentorBoard);
+    }
 
 
 }
