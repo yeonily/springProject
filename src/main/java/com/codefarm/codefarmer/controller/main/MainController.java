2736206eb998434c66007e6e79501062dfa619c0
JungHoSuk <wjdghtjr5345@gmail.com>
2022-12-14 09:10:18 +0900

    호석 메인 건드리기 시작함

--- src/main/java/com/codefarm/codefarmer/controller/main/MainController.java
+++ src/main/java/com/codefarm/codefarmer/controller/main/MainController.java
@@ -12,6 +12,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @Slf4j
 @RequestMapping("/main/*")
@@ -27,6 +30,14 @@ public class MainController {
     public void getAlbaList(Model model) {
         model.addAttribute("albas", albaListService.showListByRecentEndDate());
         model.addAttribute("programs", programListService.findTop8ByOrderByProgramApplyEndDateDesc());
+
+//        게시판 글에 해당하는 댓글 총 개수 가져오기
+        List<Long> boardIds= new ArrayList<>();
+        boardService.getBoardList().stream().map(t -> t.getBoardId()).forEach(t -> boardIds.add(t));
+        List<Long> boardReplys = new ArrayList<>();
+        boardIds.stream().map(t -> boardService.showBoardReplyCount(t)).forEach(t -> boardReplys.add(t));
+        model.addAttribute("boardReplys" , boardReplys);
+
         model.addAttribute("boards", boardService.getBoardList());
         model.addAttribute("replies", replyService.getReplyList());
     }
