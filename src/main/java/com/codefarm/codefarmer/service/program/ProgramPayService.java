22994acf7ebcdf0c6eb186c4e11a2a6999d0d688
JungHoSuk <wjdghtjr5345@gmail.com>
2022-12-18 11:24:32 +0900

    호석 프로그램 수정 진행중..

--- src/main/java/com/codefarm/codefarmer/service/program/ProgramPayService.java
+++ src/main/java/com/codefarm/codefarmer/service/program/ProgramPayService.java
@@ -1,7 +1,12 @@
 package com.codefarm.codefarmer.service.program;
 
+import com.codefarm.codefarmer.domain.member.MemberDTO;
 import com.codefarm.codefarmer.domain.program.MemberProgramDTO;
+import com.codefarm.codefarmer.domain.program.ProgramDTO;
+import com.codefarm.codefarmer.entity.member.QMember;
 import com.codefarm.codefarmer.entity.program.MemberProgram;
+import com.codefarm.codefarmer.entity.program.Program;
+import com.codefarm.codefarmer.entity.program.QProgram;
 import com.codefarm.codefarmer.repository.member.MemberRepository;
 import com.codefarm.codefarmer.repository.program.MemberProgramRepository;
 import com.codefarm.codefarmer.repository.program.ProgramRepository;
@@ -10,20 +15,47 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
+
+import static com.codefarm.codefarmer.entity.member.QMember.member;
+import static com.codefarm.codefarmer.entity.program.QProgram.program;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ProgramPayService {
 
+    private final JPAQueryFactory jpaQueryFactory;
+    private final ProgramDetailService programDetailService;
     private final MemberRepository memberRepository;
     private final ProgramRepository programRepository;
     private final MemberProgramRepository memberProgramRepository;
 
-    public void applyProgram(MemberProgramDTO memberProgramDTO,Long programId,Long memberId){
+    public int findApplyCount(Long programId){
+        return jpaQueryFactory.select(program.programApplyCount).from(program)
+                .where(program.programId.eq(programId))
+                .fetchOne();
+    }
+//    public MemberDTO findMemberByMemberId(Long memberId){
+//        jpaQueryFactory.select(new QMember(
+//
+//        ))
+//    }
+
+    public void applyProgram(MemberProgramDTO memberProgramDTO, Long programId, Long memberId){
+        int programApplyCount = findApplyCount(programId);
+        int programNewApplyCount = memberProgramDTO.getProgramApplyCount();
+        int programResultApplyCount = programApplyCount + programNewApplyCount;
+        ProgramDTO programDTO = programDetailService.showByProgramId(programId);
+        programDTO.setProgramApplyCount(programResultApplyCount);
+        Program program = programDTO.toEntity();
+
         MemberProgram memberProgram = memberProgramDTO.toEntity();
 
+        program.changeMember(memberRepository.findById(memberId).get());
         memberProgram.changeMember(memberRepository.findById(memberId).get());
         memberProgram.changeProgram(programRepository.findById(programId).get());
         memberProgramRepository.save(memberProgram);
+        programRepository.save(program);
     }
 }
