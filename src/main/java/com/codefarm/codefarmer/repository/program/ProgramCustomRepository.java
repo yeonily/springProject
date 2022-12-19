749782f6005a398cb1fbb5a27a8e834bd1feda5c
yeonily <wluds9791@gmail.com>
2022-12-19 09:52:53 +0900

    Revert "showadmin추가"
    This reverts commit eeaeb1a3444a98f6762786c3e3d3db3120569ade.

--- src/main/java/com/codefarm/codefarmer/repository/program/ProgramCustomRepository.java
+++ src/main/java/com/codefarm/codefarmer/repository/program/ProgramCustomRepository.java
@@ -10,6 +10,5 @@ public interface ProgramCustomRepository {
     public List<Program> findByProgramSearch(String keyword, String searchText);
 //    검색했을 때 글 개수
     public Integer countByProgramSearch(String keyword, String searchText);
-    //    관리자 화면
-    public List<Program> showAdmin();
+
 }
